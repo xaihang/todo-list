dd514da73ea47d02b81cbc41d149993aa012d4a5
xaihang <xaihang12@gmail.com>
2022-10-18 22:34:15 -0500

    added comments to codes

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const todos = ['buy milk', 'bake cookies', 'do laundries', 'vaccuum floor', 'cle
 //while loop is in effective system will continue to prompt asking you what you want to do until you break it with 'q or quit` 
 let input = prompt('What would you like to do?')
 
-
 //conditional statement (while loop) keep running code until "quit or q" is entered
 while (input !== 'quit' && input !== 'q') {
     if (input === 'list') {
         console.log('****************')
-        //print the to existing todos list 
+
+        //print the to existing todos list by using the for loop...loop over the arrays
         for (let i = 0; i < todos.length; i++) {
             console.log(`${i}: ${todos[i]}`);
         }
         console.log('****************')
 
-        //new list - if you want to add new todos, this condition will add `newtodos` to the existing 
+        //new list - if you want to add new todos, this condition will prompt what you want to add, and then add `newtodos` to the existing todos list 
     } else if (input === 'new') {
         const newTodos = prompt('What is the new todo?');
         todos.push(newTodos);
@@ -26,6 +26,8 @@ while (input !== 'quit' && input !== 'q') {
     } else if (input === 'delete') {
         const indexStr = parseInt(prompt('Enter an index to delete:'));
         if (!Number.isNaN(index)) {
+
+            //splice method = will remove a certain part of an array
             const deleted = todos.splice(index, 1);
             console.log(`Deleted: ${deleted[0]}`);
         } else {
