44876a5fb97529ec4df9e79c9f74106fb56def15
xaihang <xaihang12@gmail.com>
2022-10-18 21:55:03 -0500

    added arrays; list of todos

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let input = prompt('What would you like to do?')
+//arrays - things to do 
+const todo = ['buy milk', 'bake cookies', 'do laundries', 'vaccuum floor', 'clean toilet'];
 while (input !== 'quit' && input !== 'q') {
     input = prompt('What would you like to do?')
 }
