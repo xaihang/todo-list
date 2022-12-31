f987432519b3003de29de828f547b22deba9ba39
Xai Hang <xaihang12@gmail.com>
2022-12-30 23:26:25 -0600

    delecared addTo func

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
 filterOption.addEventListener('change', filterTodo);
 
+function addTodo(event) {
+    event.preventDefault();
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    
+    saveLocalTodos(todoInput.value);
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fa fa-plus-circle" aria-hidden="true"></i>';
+    completedButton.classList.add('completed-btn'); 
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+    trashButton.classList.add('trash-btn'); 
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+    todoInput.value = '';
+}
