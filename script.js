85ffde322420299f7eaf536e2819e70e79f5ca43
Xai Hang <xaihang12@gmail.com>
2022-12-30 22:46:34 -0600

    added eventlistener to todos

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('todo-list');
 const filterOption = document.querySelector('.filter.-todo');
 
+document.addEventListener('DOMContentLoaded', getLocalTodos);
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('change', filterTodo);
 
