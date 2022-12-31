c3e12d8881892aca0532cb5cdbc070ad87b79fb5
Xai Hang <xaihang12@gmail.com>
2022-12-30 22:42:22 -0600

    declared todo variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const todoInput = document.querySelector('.todo-list');
+const todoButton = document.querySelector('.todo-button'); 
+const todoList = document.querySelector('todo-list');
+const filterOption = document.querySelector('.filter.-todo');
+
+
